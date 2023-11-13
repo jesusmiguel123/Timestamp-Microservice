3ce13dbd660318c327dbea9d1f3061997cce4e12
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-11-13 17:48:45 -0500

    Solution

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+import express from "express";
+const app = express();
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+import cors from "cors";
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,15 +23,31 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:time", (req, res) => {
+  const { time } = req.params;
+  let unix, utc;
+  if(/^[0-9]+$/.test(time)){
+    unix = time;
+    utc = new Date(Number(time)).toUTCString()
+  } else {
+    let date = new Date(time);
+    unix = date.getTime();
+    utc = date.toUTCString();
+  }
+  res.json({
+    "unix": unix,
+    "utc": utc
+  });
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+const PORT = process.env.PORT ?? 3000;
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(PORT, () => {
+  console.log(`Your app is listening on http://127.0.0.1:${PORT}`);
 });
